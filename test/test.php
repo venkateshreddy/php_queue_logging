febb31c768814eb624a1069395cbfc88bb64864f
root <kvredy87@gmail.com>
2014-08-01 16:01:46 +0530

    fourth commit

--- test/test.php
+++ test/test.php
@@ -1,7 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-require_once("classes/logpublisher.php");
+/*error_reporting(E_ALL);
+ini_set('display_errors', '1');*/
+require_once("../classes/logpublisher.php");
 $rabbit_credentials	= array(
 							"host" 		=> 	"localhost",
 							"username" 	=> 	"guest",
