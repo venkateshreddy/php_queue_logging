836d7de2ac1980a264f62615c25304b3552492ee
root <kvredy87@gmail.com>
2014-08-01 15:58:40 +0530

    second commit

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once("../classes/logpublisher.php");
+$rabbit_credentials	= array(
+							"host" 		=> 	"localhost",
+							"username" 	=> 	"guest",
+							"password" 	=> 	"guest",
+							"port" 		=> 	"15672"
+							);
+echo date("Y-m-d H:i:s")."<br />";
+for($i=1; $i<=100; $i++){
+	$message_publisher = LogPublisher::getInstance($rabbit_credentials);
+	$message_publisher->publishMessage("Hai this is log message ".$i);
+}
+echo date("Y-m-d H:i:s");
+exit;
+?>
